6a42962a0cf87ec1eefc7b21ee307db1d6dfc523
gabriel feijó vilela <61162949+vilelagabrielx@users.noreply.github.com>
2022-12-29 18:16:21 -0300

    Classe de criptografia

--- CONTROLLER/cripto.py
+++ CONTROLLER/cripto.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 class Cripto:
     def __init__(self) -> None:
         
-        self.__KEY : str = 'zUN8HANMfa1njXCCfPpcvMkqfeFyrAdlgGS0ay19mS8=' #chave secreta utilizada atualmente
+        self.__KEY : str = '' #chave secreta utilizada atualmente
 
         self.__fernet = Fernet(self.__KEY) #Instancia a classe de criptografia com a chave
 
@@ -12,5 +12,4 @@ class Cripto:
 
     def decriptar(self, object : bytes) -> bytes: #função de decriptografar
         return self.__fernet.decrypt(object)
-
-# print(Cripto().encriptar(b'129.148.45.165'))
+#print(Cripto().encriptar(b'bd_bot_telegram'))
